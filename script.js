d7cd271fda92e35f2b8dcfb6d13a8f8738fa71cd
j0shyang <j0shyang@proton.me>
2024-01-01 20:33:13 -0800

    User may now hover over a square to turn it black; User may enter a new grid size

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+let gridSize = 256;
 const container = document.querySelector("#container");
 const btnContainer = document.querySelector("#btn-container");
 const newGridBtn = document.createElement("button");
 
-newGridBtn.classList.add("create-grid")
+newGridBtn.classList.add("create-grid");
 newGridBtn.textContent = "New Grid";
 btnContainer.appendChild(newGridBtn);
 
 function createGrids() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
 
-        square.addEventListener('mouseover', event => {
-            square.style.cssText = "background: black";
+        square.addEventListener("mouseover", () => {
+            square.style.background = "black";
         });
     }
 }
 
-document.addEventListener('DOMContentLoaded', event => {
-    container.textContent = '';
+createGrids();
+
+newGridBtn.addEventListener("click", () => {
+    gridSize = +prompt("Number of squares per side [1 - 100]:");
+
+    while (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+        gridSize = +prompt("Invalid input. Please enter a number from 1 - 100:");
+    }
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    gridSize = Math.pow(gridSize, 2);
+    container.textContent = "";
     createGrids();
 });
